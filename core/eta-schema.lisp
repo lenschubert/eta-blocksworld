4879f4d9e7a69a83424579be6cf578d9427cd315
bkane2 <Ben.kane@me.com>
2019-06-26 17:31:05 -0400

    Changed name to David for blocksworld

--- core/eta-schema.lisp
+++ core/eta-schema.lisp
@@ -17,7 +17,7 @@
 :Actions 
 
 ?a1. (Me say-to.v you 
-      '(Hi I am Eta\. I\'m here to help answer some spatial questions for you\.
+      '(Hi I am David\. I\'m here to help answer some spatial questions for you\.
         Please try to speak as clearly as possible\. If you wish to quit at any time\, just say bye or goodbye\.))
 
 ?a2. (Me say-to.v you 
--- rules/rules-for-question-reaction.lisp
+++ rules/rules-for-question-reaction.lisp
@@ -3,7 +3,7 @@
   (READRULES '*reaction-to-question*
   '(
     1 (0 wh_ 1 your name 0)
-      2 (Your creators have named you Eta \.) (0 :out)
+      2 (Your creators have named you David \.) (0 :out)
     1 (0 aux you 1 answer 1 question 0)
       2 (0 past question 0)
         3 (Sorry \, but you are not quite smart enough to answer follow up questions yet \.) (0 :out)
