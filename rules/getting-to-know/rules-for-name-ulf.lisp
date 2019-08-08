f51d05ddd133488006a09ecb8f46c4f007a4d8d1
bkane2 <Ben.kane@me.com>
2019-08-08 13:57:58 -0400

    Modified name ulf

--- rules/getting-to-know/rules-for-name-ulf.lisp
+++ rules/getting-to-know/rules-for-name-ulf.lisp
@@ -6,7 +6,7 @@
   (READRULES '*name-ulf-tree*
     '(
     1 (My name is 2)
-      2 (((lex-ulf! d 1) (lex-ulf! n 2) (lex-ulf! v 3) (lex-ulf! name 4))
-        ((1 2) (3 (= 4)))) (0 :ulf-recur)
+      2 (((lex-ulf! n 2) (lex-ulf! v 3) (lex-ulf! name 4))
+        ((the.d ((poss-by me.pro) 1)) (2 (= 3)))) (0 :ulf-recur)
     ))
 ); end of eval-when
